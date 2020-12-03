2b9e752cf3e07630f47bb5f69f22bc5393b69819
unknown <manuba2309@gmail.com>
2020-12-03 20:10:33 +0000

    prueba de suma y nombre de equipo

---
+++ src/Test/java/com/serverless/ResponderModelTest.java
@@ -0,0 +1,28 @@
+package com.serverless;
+
+import javafx.beans.binding.BooleanExpression;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class ResponderModelTest {
+
+    ResponderModel responder = new ResponderModel();
+
+    @Test
+    public void cuandoLaPreguntaEsVaciaDebeRetornarElNombreDelEquipo(){
+        String question = "";
+        String response = responder.answer(question);
+        assertThat(response).isEqualTo("devco");
+    }
+
+    @Test
+    public void cuandoSumo2Y2Retorna4 (){
+        String question = "56373347 What is 2 plus 2";
+        String response = responder.answer(question);
+        assertThat(response).isEqualTo("4");
+    }
+
+    @Test
+
+
+}
